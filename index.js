44c205ad5d8938e93db57b30a4e67289bada04b6
Kyle Geske <stungeye@gmail.com>
2019-01-05 06:22:37 -0600

    Manual demo with 9 questions.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import ABCJS from 'abcjs';
 
 const abc = '|C2D2E2|';
+const scaler = 2;
 
-ABCJS.renderAbc('paper', abc, { scale: 2 });
+ABCJS.renderAbc('paper-1', '|C2D2E2|', { scale: scaler });
+ABCJS.renderAbc('paper-2', '|E2F2G2|', { scale: scaler });
+ABCJS.renderAbc('paper-3', '|C2D2C2|', { scale: scaler });
+ABCJS.renderAbc('paper-4', '|E2D2C2|', { scale: scaler });
+ABCJS.renderAbc('paper-5', '|C2E2C2|', { scale: scaler });
+ABCJS.renderAbc('paper-6', '|G2F2E2|', { scale: scaler });
+ABCJS.renderAbc('paper-7', '|C2E2G2|', { scale: scaler });
+ABCJS.renderAbc('paper-8', '|G2E2C2|', { scale: scaler });
+ABCJS.renderAbc('paper-9', '|G2F2C2|', { scale: scaler });
